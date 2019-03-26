d972785ba764ded3e3d765e87cc425cbfafdf286
Matthew Blanke <blanke.matt@gmail.com>
2019-03-26 16:31:49 -0700

    merged

--- Test.m
+++ Test.m
@@ -1,11 +1,7 @@
 clc;clear;close all;
 
 clock1 = imread("images/clock2.jpg");
-<<<<<<< HEAD
 [twelve,six,nine,three, I12, I6, I9, I3] = findAxes(clock1);
 Im2 = findClockCenter(clock1,twelve,three,six,nine);
 imshow(Im2);
-=======
-Im2 = findClockCenter(clock1);
-[twelve,six,nine,three] = findAxes(Im2);
->>>>>>> 86db34e674aa80bbba16cd025499098f9f109147
+
--- findClockCenter.m
+++ findClockCenter.m
@@ -1,4 +1,3 @@
-
 function ISkew = findClockCenter(IClock)
     ISkew = IClock;
 end
