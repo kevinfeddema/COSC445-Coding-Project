caaf2c37f0634fc597c3f83589b51fb4435ab5b1
Trevor Gallicano <trevorg@hotmail.ca>
2019-03-27 22:40:08 -0700

    Added headers to fixClockPerspective and FindClockCenter

--- Test.m
+++ Test.m
@@ -5,5 +5,7 @@ clock1 = fixClockPerspective(clock1);
 [twelve,six,nine,three, I12, I6, I9, I3] = findAxes(clock1);
 [centerX,centerY] = findClockCenter(twelve,six,nine,three);
 
+
+
 imshow(clock1);
 
