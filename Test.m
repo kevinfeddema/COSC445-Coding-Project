d41f0560908edb3eb9f921df967ccf7a6d80c861
Trevor Gallicano <trevorg@hotmail.ca>
2019-03-31 13:42:18 -0700

    Added Clock images

--- Test.m
+++ Test.m
@@ -5,7 +5,14 @@ clock1 = fixClockPerspective(clock1);
 [twelve,six,nine,three, I12, I6, I9, I3] = findAxes(clock1);
 [centerX,centerY] = findClockCenter(twelve,six,nine,three);
 
+low_thresh = 0.08; 
+high_thresh = 0.2; 
+sigma = 1;
 
+%edges = getEdges(clock1,low_thresh,high_thresh,sigma);
+edges = rgb2gray(clock1);
+edges = edge(edges, 'Canny');
+getHoughLines(edges);
 
 imshow(clock1);
 
